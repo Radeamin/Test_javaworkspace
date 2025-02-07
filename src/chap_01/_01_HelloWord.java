76828c0ccb0c9222e6e703c5dff2f02dd54a2e21
Radeamin <leerungah@gmail.com>
2025-02-07 22:54:39 +0900

    자료형과 변수

---
+++ src/chap_01/_01_HelloWord.java
@@ -0,0 +1,8 @@
+package chap_01;
+
+public class _01_HelloWord {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+    }
+}
+
---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,24 @@
+package chap_01;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        int time_3 = 3;
+        int time_5 = 5;
+
+        String _01_busNo1 = "1234";
+        String _02_busNO2 = "상암08";
+
+        double _01_Kmb = 1.5;
+        double _02_Kmb = 0.8;
+        double _03_Kmb = 1.2;
+        System.out.println(_02_busNO2 + "번 버스 ");
+        System.out.println("약 " +time_3+"분 후 도착");
+
+        System.out.println("남은 거리 " + _03_Kmb + "Km");
+
+    }
+   
+    
+}
