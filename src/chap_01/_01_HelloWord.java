251fe28d18aeeae306ee4c38587391dcf292db9c
Radeamin <leerungah@gmail.com>
2025-02-07 23:00:05 +0900

    반갑습니다 추가(git commit 연습)

--- src/chap_01/_01_HelloWord.java
+++ src/chap_01/_01_HelloWord.java
@@ -3,6 +3,8 @@ package chap_01;
 public class _01_HelloWord {
     public static void main(String[] args) {
         System.out.println("안녕하세요");
+        //추가
+        System.out.println("반갑습니다.");
     }
 }
 
